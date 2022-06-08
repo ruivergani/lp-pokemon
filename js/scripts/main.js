2404f506861ec8e1a7c6050123d68f0d2fe93021
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-06-08 14:37:21 +0100

    listing pokemons function

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -32,4 +32,24 @@ btnDropdownSelect.addEventListener('click', () => { // arrow function
   // or could have done btnDropdownSelect.parentElement.classList.toggle('active);
 })
 
+// basically the API has different URL for listing or filtering or doing other activities with the pokemons, that is the reason you use the parameter URL in the listing of Pokemons
 
+// Function to list all pokemons
+function listingPokemons(urlApi){ // url from API
+    // using axios (same as fetch)
+    axios({
+      method: 'GET', 
+      url: urlApi
+    })
+    .then((response) => {
+      console.log(response)
+    })
+}
+
+// load page => load function
+listingPokemons('https://pokeapi.co/api/v2/pokemon?limit=9&offset=0')
+
+
+
+
+// API para listar pokemon: https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0 (limit per page - offset is start from)
